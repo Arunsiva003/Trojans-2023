96e18e220c8f0416fd9f07a109a87920a65ba121
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-08-04 20:43:13 +0530

    sec commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import { Router as BrowserRouter, Router, Route, Routes } from "react-router-dom";
-import HomePage from './Pages/HomePage';
-import RegistrationPage from './Pages/RegistrationPage';
-import EventsPage from './Pages/EventsPage';
-import SponsorsPage from './Pages/SponsorsPage';
-import ContactPage from './Pages/ContactPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './Pages/HomePage.js';
+import RegistrationPage from './Pages/RegistrationPage.js';
+import EventsPage from './Pages/EventsPage.js';
+import SponsorsPage from './Pages/SponsorsPage.js';
+import ContactPage from './Pages/ContactPage.js';
 
 function App() {
-	return (
-    <div>
-    {" "}
-    {/* <Navbar /> */}
+  return (
     <Router>
-      <Routes>
-        <Route path="/home" element={<HomePage />}></Route>
-        <Route path="/sponsors" element={<SponsorsPage />}></Route>
-        <Route path="/registration" element={<RegistrationPage />}></Route>
-        <Route path="/events" element={<EventsPage />}></Route>
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sponsors" element={<SponsorsPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </Router>
-  </div>
-	);
+  );
 }
+
 
 export default App;
