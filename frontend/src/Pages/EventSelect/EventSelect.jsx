8ce2e8033328e43a5ab7b2519058f25d9feb8051
jj-vinaysaran <vinayjayjay12@gmail.com>
2023-08-06 09:32:46 +0530

    Events Select complete

--- frontend/src/Pages/EventSelect/EventSelect.jsx
+++ frontend/src/Pages/EventSelect/EventSelect.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./eventselect.css"
+import TechImg from '../../Images/Tech/Tech.png';
+import NonTechImg from '../../Images/NonTech/NonTech.png';
 function EventSelect() {
   return (
-    <div>
-      <h1>event select</h1>
+    <div className='events_home'>
+      <div className='left_tech'>
+        <h1 className='events_heading'>TECHNICAL</h1>
+        <img src={TechImg} className='events_img'/>
+        <h2 className='run_btn'><a>Run</a></h2>
+      </div>
+      <div className='left_tech kela_cls'>
+        <h1 className='events_heading'>NON-TECHNICAL</h1>
+        <img src={NonTechImg} className='events_img'/>
+        <h2 className='run_btn'><a>Run</a></h2>
+
+      </div>
     </div>
   )
 }
