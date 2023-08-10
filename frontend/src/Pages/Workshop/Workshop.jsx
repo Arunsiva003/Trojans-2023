ad1efc0e12eb6680a8bdb5307b3aa216f017a3d6
jj-vinaysaran <vinayjayjay12@gmail.com>
2023-08-10 17:11:07 +0530

    commit

--- frontend/src/Pages/Workshop/Workshop.jsx
+++ frontend/src/Pages/Workshop/Workshop.jsx
@@ -79,22 +79,26 @@ timing:event.timing } });
     return (
       <div>
          <div className='Workshop_cont'>
-        <div className='Workshop_title'>
-          <h1 className='mobile-view'>{"< Workshop />"}</h1>
-        </div>
-        <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" div "}</span><span className='Work_tag'>{">"}</span></h1>
-        <div className='all-workshop'>
-        {workshopdata.map((event, index) => (
-          
-            <div className='Workshop_cards' key={index}>
-              <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" Img "}</span><span className='Work_tag'>{">"}</span></h1>
-            <img className="Work_img" src={event.imageSrc} alt="Event" />
-            <h2 className='Work_close Work_tag'>{"/>"}</h2>
-            <h2 className='Work_view' onClick={()=>{sharedata(event)}}><span className='Work_tag'>{"< "}</span> <span className='Work_tag_name'>{"a "}</span><a className='Work_view_btn'>{event.buttonText}</a><span className='Work_tag'>{" />"}</span> </h2>
+            <div className='Workshop_title'>
+              <h1 className='mobile-view'>{"< Workshop />"}</h1>
+            </div>
+            <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" div "}</span><span className='Work_tag'>{">"}</span></h1>
+            <div className='all-workshop'>
+              {workshopdata.map((event, index) => (
+            
+              <div className='Workshop_cards' key={index}>
+                  <div className="work_circle"></div>
+                  <div className="work_circle"></div>
+                  <div className="work_card_in">
+                    <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" Img "}</span><span className='Work_tag'>{">"}</span></h1>
+                    <img className="Work_img" src={event.imageSrc} alt="Event" />
+                    <h2 className='Work_close Work_tag'>{"/>"}</h2>
+                    <h2 className='Work_view' onClick={()=>{sharedata(event)}}><span className='Work_tag'>{"< "}</span> <span className='Work_tag_name'>{"a "}</span><a className='Work_view_btn'>{event.buttonText}</a><span className='Work_tag'>{" />"}</span> </h2>
+                  </div>
+              </div>
+            ))}
           </div>
-        ))}
-        </div>
-        <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" /div "}</span><span className='Work_tag'>{">"}</span></h1>
+          <h1 style={{margin:"10px"}}><span className='Work_tag'>{"<"}</span><span className='Work_tag_name'>{" /div "}</span><span className='Work_tag'>{">"}</span></h1>
         </div>
       </div>
     )
